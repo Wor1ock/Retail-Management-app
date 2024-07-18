42608f4156b41b27071ba128ab5bfbf680ba5b5e
Wor1ock <vanya.yankin9@gmail.com>
2024-07-18 17:35:32 +0400

    feat: добавить в редактор магазина вкладку Products для добавления и просмотра товаров

--- modules/web/src/com/company/intership/web/screens/store/StoreEdit.java
+++ modules/web/src/com/company/intership/web/screens/store/StoreEdit.java
@@ -1,11 +1,23 @@
 package com.company.intership.web.screens.store;
 
+import com.company.intership.entity.ProductInStore;
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.intership.entity.Store;
+
+import javax.inject.Inject;
 
 @UiController("intership_Store.edit")
 @UiDescriptor("store-edit.xml")
 @EditedEntityContainer("storeDc")
 @LoadDataBeforeShow
 public class StoreEdit extends StandardEditor<Store> {
+    @Inject
+    private CollectionLoader<ProductInStore> productsDl;
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        productsDl.setParameter("storeId", getEditedEntity().getId());
+        productsDl.load();
+    }
 }
