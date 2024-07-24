440fab10f19e66e194ad8af8c12867e7e89a6dd2
Wor1ock <vanya.yankin9@gmail.com>
2024-07-25 02:29:53 +0400

    feat: отформатировать запросы в сервисе ProductManufacturer

--- modules/core/src/com/company/intership/service/ProductManufacturerServiceBean.java
+++ modules/core/src/com/company/intership/service/ProductManufacturerServiceBean.java
@@ -6,7 +6,6 @@ import com.company.intership.entity.ProductManufacturer;
 import com.company.intership.entity.Store;
 import com.haulmont.cuba.core.EntityManager;
 import com.haulmont.cuba.core.Persistence;
-import com.haulmont.cuba.core.TypedQuery;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,51 +23,41 @@ public class ProductManufacturerServiceBean implements ProductManufacturerServic
     @Transactional
     public List<ProductInStore> getProductsWithLowQuantity(ProductManufacturer manufacturer, UUID storeId, int threshold) {
         EntityManager entityManager = persistence.getEntityManager();
-        String query = "select p from intership_ProductInStore p " +
-                "where p.store.id = :storeId " +
-                "and p.product.manufacturer = :manufacturer " +
-                "and (p.quantity = 0 or p.quantity < :threshold)";
-        TypedQuery<ProductInStore> typedQuery = entityManager.createQuery(query, ProductInStore.class);
-        typedQuery.setViewName("productInStore-view");
-        typedQuery.setParameter("storeId", storeId);
-        typedQuery.setParameter("manufacturer", manufacturer);
-        typedQuery.setParameter("threshold", threshold);
-
-        List<ProductInStore> result = typedQuery.getResultList();
-        return result;
+        return entityManager.createQuery(
+                        "SELECT p FROM intership_ProductInStore p " +
+                                "WHERE p.store.id = :storeId " +
+                                "AND p.product.manufacturer = :manufacturer " +
+                                "AND (p.quantity = 0 OR p.quantity < :threshold)", ProductInStore.class)
+                .setViewName("productInStore-view")
+                .setParameter("storeId", storeId)
+                .setParameter("manufacturer", manufacturer)
+                .setParameter("threshold", threshold)
+                .getResultList();
     }
 
     @Override
     @Transactional
     public List<ProductInStore> getProductsWithLowQuantityFromAllStores(ProductManufacturer manufacturer, int threshold) {
         EntityManager entityManager = persistence.getEntityManager();
-        String query = "select p from intership_ProductInStore p " +
-                "where p.product.manufacturer = :manufacturer " +
-                "and (p.quantity = 0 or p.quantity < :threshold)";
-        TypedQuery<ProductInStore> typedQuery = entityManager.createQuery(query, ProductInStore.class);
-        typedQuery.setViewName("productInStore-view");
-        typedQuery.setParameter("manufacturer", manufacturer);
-        typedQuery.setParameter("threshold", threshold);
-
-        List<ProductInStore> result = typedQuery.getResultList();
-        return result;
+        return entityManager.createQuery(
+                        "SELECT p FROM intership_ProductInStore p " +
+                                "WHERE p.product.manufacturer = :manufacturer " +
+                                "AND (p.quantity = 0 OR p.quantity < :threshold)", ProductInStore.class)
+                .setViewName("productInStore-view")
+                .setParameter("manufacturer", manufacturer)
+                .setParameter("threshold", threshold)
+                .getResultList();
     }
 
     @Override
     @Transactional
     public List<Store> getStoresWithoutProduct(Product product) {
         EntityManager entityManager = persistence.getEntityManager();
-        String query = "select s from intership_Store s " +
-                "where not exists (select 1 from intership_ProductInStore p " +
-                "where p.store.id = s.id " +
-                "and p.product = :product and " +
-                "(p.quantity = 0 or p.quantity > 0))";
-        TypedQuery<Store> typedQuery = entityManager.createQuery(query, Store.class);
-        typedQuery.setViewName("store-view");
-        typedQuery.setParameter("product", product);
-
-        List<Store> result = typedQuery.getResultList();
-        return result;
+        return entityManager.createQuery("SELECT s FROM intership_Store s WHERE not exists " +
+                        "(SELECT 1 FROM intership_ProductInStore p WHERE p.store.id = s.id " +
+                        "AND p.product = :product AND (p.quantity = 0 OR p.quantity > 0))", Store.class)
+                .setViewName("store-view")
+                .setParameter("product", product)
+                .getResultList();
     }
-
 }
